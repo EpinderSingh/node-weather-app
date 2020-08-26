3b9b3bfb8e9f0b68864377d0672c6279cfb13180
Epinder <epinder.singh@gmail.com>
2020-08-26 16:41:05 +0800

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,25 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+  const url =
+    'http://api.weatherstack.com/current?access_key=7a9e3a58bb5dec083665d92dc698d091&query=' +
+    latitude +
+    ',' +
+    longitude +
+    '&units=m';
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect weather services!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degree out. It feel like ${body.current.feelslike} degree out.`,
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
